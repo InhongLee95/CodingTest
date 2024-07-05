ed37e5548718404e27ef16d576b3679709d16e80
InhongLee95 <134349722+InhongLee95@users.noreply.github.com>
2024-07-05 15:31:58 +0900

    [level 0] Title: 수 조작하기 2, Time: 647.67 ms, Memory: 378 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -3,18 +3,17 @@ import java.util.*;
 class Solution {
     public String solution(int[] numLog) {
                 
-        StringBuilder sb = new StringBuilder();
+        String answer = "";
         
         
         for(int i = 0; i<numLog.length-1; i++) {
-            if(numLog[i] + 1 == numLog[i+1] ) sb.append("w");
-            if(numLog[i] - 1 == numLog[i+1] ) sb.append("s");
-            if(numLog[i] + 10 == numLog[i+1] ) sb.append("d");
-            if(numLog[i] - 10 == numLog[i+1] ) sb.append("a");
+            if(numLog[i] + 1 == numLog[i+1] ) answer += "w";
+            if(numLog[i] - 1 == numLog[i+1] ) answer += "s";
+            if(numLog[i] + 10 == numLog[i+1] ) answer += "d";
+            if(numLog[i] - 10 == numLog[i+1] ) answer += "a";
         
         }
         
-        String answer = sb.toString();
         
         
         return answer;
