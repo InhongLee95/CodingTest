e29855070bac647abf06accad3d5a3e471d242bf
InhongLee95 <134349722+InhongLee95@users.noreply.github.com>
2024-07-15 01:10:35 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 1.42 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String[] solution(String my_string) {
+        my_string = my_string.strip();
+        
+        String[] answer = my_string.split("\\s+");
+            
+        
+        
+        
+        
+        return answer;
+    }
+}
