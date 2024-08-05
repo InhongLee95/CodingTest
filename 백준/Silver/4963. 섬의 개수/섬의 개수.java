66ba52dde28e8444017ef29d97b81a2d65aae3ea
InhongLee95 <134349722+InhongLee95@users.noreply.github.com>
2024-08-05 20:30:08 +0900

    [Silver II] Title: 섬의 개수, Time: 136 ms, Memory: 16008 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -5,73 +5,81 @@ public class Main {
 
     static int arr[][];
     static boolean check[][];
-    static int dirX[] = {1,-1,0,0,1,1,-1,-1};
-    static int dirY[] = {0,0,1,-1,1,-1,-1,1};
-    static int w, h, nowX, nowY;
+    static int count;
+    static int[] dirX = {1,-1,0,0,1,1,-1,-1};
+    static int[] dirY = {0,0,1,-1,1,-1,1,-1};
+    static int nowX, nowY, w,h;
 
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         String str = " ";
 
-
-        // 0 0 나올 때까지 무한 입력
-        while (!(str = br.readLine()).equals("0 0")) {
+        // 0 0 나오기 전까지 무한 입력
+        while(!(str = br.readLine()).equals("0 0")) {
             st = new StringTokenizer(str);
-            w = Integer.parseInt(st.nextToken()); // 너비
-            h = Integer.parseInt(st.nextToken()); // 높이
+
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+
             arr = new int[h][w];
             check = new boolean[h][w];
 
-            for (int i = 0; i < h; i++) {
+            for(int i = 0; i<h; i++) {
                 st = new StringTokenizer(br.readLine());
 
-                for (int j = 0; j < w; j++) {
+                for(int j = 0; j<w; j++){
                     arr[i][j] = Integer.parseInt(st.nextToken());
                 }
-            }
+            }//for
+
+            // 섬개수 측정 for문
+            int islrand_count = 0;
+            for(int i = 0; i<h; i++) {
+                for(int j = 0; j<w; j++) {
+                    if(arr[i][j] == 1 && !check[i][j]) {
+                        islrand_count++;
+                        dfs(i,j);
+                    }
 
 
-        int island_count = 0;
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-
-                if (!check[i][j] && arr[i][j] == 1) {
-                    island_count++;
-                    dfs(i, j);
                 }
+            }//for
 
-            }
-        }//for
-
-        sb.append(island_count).append('\n');
+            sb.append(islrand_count).append('\n');
 
         }//while
 
         System.out.println(sb);
+
     }//main
 
+    public static void dfs(int h, int w) {
+        check[h][w] = true;
 
-    public static void dfs(int x, int y) {
-        check[x][y] = true;
 
-        for(int i = 0; i<8; i++){
-            nowX = dirX[i] + x;
-            nowY = dirY[i] + y;
+        for(int i = 0; i<8; i++) {
+            nowX = dirX[i] + w;
+            nowY = dirY[i] + h;
 
-            if(range_check() && !check[nowX][nowY] && arr[nowX][nowY]  ==1) {
-                dfs(nowX, nowY);
+            if(island_scope()  && !check[nowY][nowX] && arr[nowY][nowX] == 1 ) {
+                dfs(nowY, nowX);
             }
+
         }
 
+
+    }//dfs
+
+    public static boolean island_scope() {
+        return (nowX >= 0  && nowY >= 0 && nowX <w && nowY < h);
+
     }
 
-    public static boolean range_check() {
-        return (nowX >=0 && nowY >=0 && nowX < h && nowY <w );
-    }
+
 
 
 }
