e10854ba760c66af2af284603ca2273c47e486df
InhongLee95 <134349722+InhongLee95@users.noreply.github.com>
2024-09-07 21:10:29 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,40 +1,39 @@
-import java.util.*;
+
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
-
-
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[9];
-        int sum = 0;
 
-        // 배열 입력
+
+        int[] arr = new int[9];
+        int sum  = 0;
+
         for(int i = 0; i<9; i++) {
-            int N = Integer.parseInt(br.readLine());
-            arr[i] = N;
-            sum += N;
+            int n = Integer.parseInt(br.readLine());
+            arr[i] = n;
+            sum += arr[i];
         }
 
         boolean flag = false;
-
         for(int i = 0; i<8; i++) {
             for(int j = i+1; j<9; j++) {
-                if(sum - arr[i] - arr[j] == 100) {
+                if(sum - arr[i] - arr[j] == 100){
                     arr[i] = 0;
                     arr[j] = 0;
                     flag = true;
                     break;
                 }
+
+            if(flag) break;
+
             }
-            if(flag == true) break;
-        }
+        }//for
 
         Arrays.sort(arr);
 
-        for(int i=2; i<arr.length; i++) {
+        for(int i = 2; i<9; i++) {
             System.out.println(arr[i]);
         }
 
