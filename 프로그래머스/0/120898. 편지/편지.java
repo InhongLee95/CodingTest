2b45c815f698ddaa795a7fb6131d3127826b8592
InhongLee95 <134349722+InhongLee95@users.noreply.github.com>
2024-09-15 19:07:18 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String message) {
+        
+        
+        int answer = 2 * message.length();
+        
+        
+        
+        
+        return answer;
+    }
+}
