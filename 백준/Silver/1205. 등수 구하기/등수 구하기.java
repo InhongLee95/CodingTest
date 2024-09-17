b545314dd89ee23a09e0f51d23d07e1544824fbd
InhongLee95 <134349722+InhongLee95@users.noreply.github.com>
2024-09-17 23:52:38 +0900

    [Silver IV] Title: 등수 구하기, Time: 96 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.java
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.java
@@ -26,31 +26,26 @@ public class Main {
             scores[i] = score;
         }
 
-        int result = solution(scores, n, newScore, p);
-        System.out.println(result);
 
+        if(n == p && newScore <= scores[scores.length-1]) {
+            System.out.println(-1);
+        }
+        else{
+            int rank = 1;
 
-    }
-
-    private static int solution(int[] scores, int n, int newScore, int p) {
-
-        // 랭킹리스트가 가득차있으며, 가장 작은값보다 작거나 같다면 못넣음
-        if(n==p && scores[n-1] >= newScore)
-            return -1;
-
-        // 랭킹리스트에서 자기 위치 찾기
-        int rank = 1;
-        int prev = -1;
-        for(int i=0;i<n;i++) {
-            if(scores[i] > newScore){
-                // 같은 값이 여러개에 있을 경우, 맨 앞에 하나에 대해서만 rank 증가
-                if(scores[i] == newScore && prev == newScore)
-                    continue;
-                rank++;
+            for(int i = 0; i<scores.length; i++) {
+                if (scores[i] > newScore) rank++;
+                else {
+                    break;
+                }
             }
-            prev = scores[i];
-        }//for
-        return rank;
+             System.out.println(rank);
+
+        }
+
+
+
     }
+
 
 }
