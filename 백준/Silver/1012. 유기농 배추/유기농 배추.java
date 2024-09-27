5e061e9e3da47f6adbdc744c948e91f301f1d5ff
InhongLee95 <134349722+InhongLee95@users.noreply.github.com>
2024-09-27 11:24:19 +0900

    [Silver II] Title: 유기농 배추, Time: 136 ms, Memory: 15824 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,89 +1,78 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N, M, K;
-    static boolean[][] check;
+
+    static int x,y,c;
     static int[][] arr;
-    static int count;
-    static int dirX[] = {1,-1,0,0};
-    static int dirY[] = {0,0,1,-1};
+    static boolean[][] cheack;
+    static int[] dirX = {0, 0, -1, 1};
+    static int[] dirY = {1, -1, 0, 0};
     static int nowX, nowY;
 
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int test = Integer.parseInt(br.readLine());
-        StringTokenizer st;
+        int t = Integer.parseInt(br.readLine());
 
 
-        // 테스트 횟수
-        for(int t = 0; t<test; t++) {
-            st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken());
+        for(int k = 0; k <t; k++) {
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
 
-            arr = new int[N][M];
-            check = new boolean[N][M];
+            arr = new int[x][y];
+            cheack = new boolean[x][y];
+            int count = 0;
 
-
-            // 배열 입력
-            for(int i = 0; i<K; i++) {
+            for (int i = 0; i < c; i++) {
                 st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
 
-                arr[x][y] = 1;
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+
+                arr[a][b] = 1;
             }
 
-            count = 0;
-
-            // dfs 호출
-            for(int i = 0; i<N; i++) {
-                for(int j = 0; j<M; j++) {
-                    if(!check[i][j] && arr[i][j] ==1) {
-                        dfs(i, j);
+            for(int i = 0; i<x; i++) {
+                for(int j =0; j<y; j++) {
+                    if(!cheack[i][j] && arr[i][j] ==1) {
+                        dfs(i,j);
                         count++;
-
                     }
 
                 }
-
-            }//for - dfs
-
-
+            }
 
             System.out.println(count);
 
-        }//for
+        }//t-for
 
+    }
 
-
-    }//main
 
 
     static private void dfs(int x, int y) {
-        check[x][y] = true;
 
-        for(int i =0; i<4; i++) {
+        cheack[x][y] = true;
+
+        for(int i = 0; i<4; i++) {
             nowX = dirX[i] + x;
             nowY = dirY[i] + y;
 
-            if(rangeCheck() && !check[nowX][nowY] && arr[nowX][nowY]==1) {
-                dfs(nowX, nowY);
+            if(checkRange() && !cheack[nowX][nowY] && arr[nowX][nowY] == 1) {
+                dfs(nowX,nowY);
             }
-
         }
-
-
 
 
     }
 
-    static private boolean rangeCheck() {
-        if(nowX >=0 && nowY >=0 && nowX <N && nowY <M) return true;
+    static private boolean checkRange() {
+        if(nowX >=0 && nowY >=0 && nowX <x && nowY <y) return true;
         else return false;
     }
 
