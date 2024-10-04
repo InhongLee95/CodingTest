513f03a8f54c21ccd9b95f6da5260170a085c1fb
InhongLee95 <134349722+InhongLee95@users.noreply.github.com>
2024-10-05 05:05:08 +0900

    [level 1] Title: 모의고사, Time: 0.68 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -5,39 +5,30 @@ class Solution {
         int[] score = {0,0,0};
         
         int[] one = {1, 2, 3, 4, 5};
-        int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] two = {2,1,2,3,2,4,2,5};
+        int[] three = {3,3,1,1,2,2,4,4,5,5};
+        int max = 0;
         
-        
-        // 점수 체크
-        for(int i = 0; i< answers.length; i++) {
-            if(answers[i] == one[i%5]) score[0]++;
-            if(answers[i] == two[i%8]) score[1]++;
-            if(answers[i] == three[i%10]) score[2]++;            
+        for(int i = 0; i<answers.length; i++) {
+            if(one[i%5] == answers[i]) score[0]++;
+            if(two[i%8] == answers[i]) score[1]++;
+            if(three[i%10] == answers[i]) score[2]++;
+            
         }
+        max = Math.max(score[0], Math.max(score[1], score[2]));
         
-        
-        // 최고 점수 값 
-        int max = Math.max(score[0], Math.max(score[1], score[2]));
-        
-        // 최고 점수자 선별
         ArrayList<Integer> list = new ArrayList<>();
-        for(int i = 0; i< score.length; i++) {
-            if(max == score[i]) list.add(i+1);
+        for(int i = 0; i<score.length; i++) {
+            if(score[i] == max) {
+                list.add(i+1);
+            }
         }
         
-        
-        // 최다 점수획득자가 2명이상일 경우 수험자 번호 오름차순으로 정렬
-        Collections.sort(list);
-        
-        //최고 점수획득자 타입변환
         int[] answer = new int[list.size()];
-        
-        for(int i = 0; i<answer.length; i++) {
+        for(int i = 0; i< answer.length; i++) {
             answer[i] = list.get(i);
+            
         }
-        
-        
         
         
         
